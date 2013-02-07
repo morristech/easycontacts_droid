0a191a592a2e130caa38a89e0540b3851c602360
Andrew Kornilov <frutik@gmail.com>
2013-02-07 20:51:25 +0200

    added http client

--- src/com/easycontacts/agent/MainActivity.java
+++ src/com/easycontacts/agent/MainActivity.java
@@ -1,5 +1,36 @@
 package com.easycontacts.agent;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import java.util.UUID;
+import com.loopj.android.http.*;
 
-public class MainActivity extends Activity {}
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	  }
+	
+	public void doRequest(View view) {
+		AsyncHttpClient client = new AsyncHttpClient();
+
+        UUID uuid = java.util.UUID.randomUUID();
+
+		RequestParams params = new RequestParams();
+		params.put("c", "4309133");
+		params.put("e", "380352405277");
+		params.put("a", "SIP/1117");
+		params.put("i", uuid.toString());
+		
+		client.post("http://easy-contacts.com:8888/popup/ede1e57a-790a-11e1-9a59-78e400edfc41", params, 
+				new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(String response) {
+            	Log.w("DEBUG", response);
+            }
+        });
+	}
+}
--- src/com/easycontacts/agent/StateReceiver.java
+++ src/com/easycontacts/agent/StateReceiver.java
@@ -1,11 +1,17 @@
 package com.easycontacts.agent;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+
+import java.util.UUID;
 
 public class StateReceiver extends BroadcastReceiver {
 
@@ -16,9 +22,31 @@ public class StateReceiver extends BroadcastReceiver {
       String state = extras.getString(TelephonyManager.EXTRA_STATE);
       Log.w("DEBUG", state);
       if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-        String phoneNumber = extras
-            .getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        String phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        
         Log.w("DEBUG", phoneNumber);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        String serverKey = "ede1e57a-790a-11e1-9a59-78e400edfc41";
+        String myNumber = "380352405277";
+        String myAgentKey = "SIP/1117";
+		
+        UUID uuid = java.util.UUID.randomUUID();
+        
+		RequestParams params = new RequestParams();
+		params.put("c", phoneNumber);
+		params.put("e", myNumber);
+		params.put("a", myAgentKey);
+		params.put("i", uuid.toString());
+		
+		client.post("http://easy-contacts.com:8888/popup/" + serverKey, params, 
+				new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(String response) {
+            	Log.w("DEBUG", response);
+            }
+        });
       }
     }
   }
