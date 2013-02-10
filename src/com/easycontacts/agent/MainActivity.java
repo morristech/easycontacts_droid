f50130c14d8a836aa4744185657b4a93f6e13ce9
Andrew Kornilov <frutik@gmail.com>
2013-02-10 22:37:29 +0200

    -

--- src/com/easycontacts/agent/MainActivity.java
+++ src/com/easycontacts/agent/MainActivity.java
@@ -6,26 +6,35 @@ import android.view.View;
 import android.util.Log;
 import java.util.UUID;
 import com.loopj.android.http.*;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PreferenceActivity {
+	SharedPreferences preferences;
+	
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    addPreferencesFromResource(R.xml.preferences);
 	    setContentView(R.layout.main);
 	  }
-	
+
 	public void doRequest(View view) {
 		AsyncHttpClient client = new AsyncHttpClient();
 
         UUID uuid = java.util.UUID.randomUUID();
 
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);	
+        
 		RequestParams params = new RequestParams();
 		params.put("c", "4309133");
-		params.put("e", "380352405277");
-		params.put("a", "SIP/1117");
+		params.put("e", preferences.getString("Channel", ""));
+		params.put("a", preferences.getString("AgentKey", ""));
 		params.put("i", uuid.toString());
 		
-		client.post("http://easy-contacts.com:8888/popup/ede1e57a-790a-11e1-9a59-78e400edfc41", params, 
+		client.post("http://easy-contacts.com:8888/popup/" + preferences.getString("ServerKey", ""), params, 
 				new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(String response) {
--- src/com/easycontacts/agent/StateReceiver.java
+++ src/com/easycontacts/agent/StateReceiver.java
@@ -7,6 +7,7 @@ import com.loopj.android.http.RequestParams;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -14,7 +15,6 @@ import android.util.Log;
 import java.util.UUID;
 
 public class StateReceiver extends BroadcastReceiver {
-
   @Override
   public void onReceive(Context context, Intent intent) {
     Bundle extras = intent.getExtras();
@@ -22,17 +22,19 @@ public class StateReceiver extends BroadcastReceiver {
       String state = extras.getString(TelephonyManager.EXTRA_STATE);
       Log.w("DEBUG", state);
       if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+    	  
+    	SharedPreferences preferences = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);  
+    	  
         String phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+        String serverKey = preferences.getString("ServerKey", "");
+        String myNumber = preferences.getString("Channel", "");
+        String myAgentKey = preferences.getString("AgentKey", "");		
+
+        UUID uuid = java.util.UUID.randomUUID();
         
         Log.w("DEBUG", phoneNumber);
 
         AsyncHttpClient client = new AsyncHttpClient();
-
-        String serverKey = "ede1e57a-790a-11e1-9a59-78e400edfc41";
-        String myNumber = "380352405277";
-        String myAgentKey = "SIP/1117";
-		
-        UUID uuid = java.util.UUID.randomUUID();
         
 		RequestParams params = new RequestParams();
 		params.put("c", phoneNumber);
@@ -40,7 +42,8 @@ public class StateReceiver extends BroadcastReceiver {
 		params.put("a", myAgentKey);
 		params.put("i", uuid.toString());
 		
-		client.post("http://easy-contacts.com:8888/popup/" + serverKey, params, 
+		client.post("http://easy-contacts.com:8888/popup/" + serverKey, 
+				params, 
 				new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(String response) {
